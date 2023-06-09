f7670886e21c88aadffc8ef5e086678c6318a879
Jose Antonio Machaca Flores <135687051+josepkmz@users.noreply.github.com>
2023-06-09 10:17:34 -0500

    CORREGIDO entradas_de_concierto.cpp

--- entradas_de_concierto.cpp
+++ entradas_de_concierto.cpp
@@ -1,8 +1,9 @@
+
 #include <iostream>
 using namespace std;
 
 int main() {
-  int ubicacion, cantidad, precio, total;
+    int ubicacion, cantidad, precio, total;
     string bebida;
     cout << "Ubicaciones disponibles:" << endl;
     cout << "1. General - 50" << endl;
@@ -11,39 +12,47 @@ int main() {
     cout << "4. VIP - 160" << endl;
     cout << "Ingrese la ubicación deseada (1-4): ";
     cin >> ubicacion;
-
     cout << "Ingrese la cantidad de entradas: ";
     cin >> cantidad;
-
     switch (ubicacion) {
-        case 1: cout<<"ud. eligio general: "<<endl;
+        case 1:
+            cout << "Ud. eligió general." << endl;
             precio = 50;
             break;
-        case 2: cout<<"ud. escogio preferencial: "<<endl;
+        case 2:
+            cout << "Ud. eligió preferencial." << endl;
             precio = 80;
             break;
-        case 3: cout<<"ud. eligio platea: "<<endl;
+        case 3:
+            cout << "Ud. eligió platea." << endl;
             precio = 120;
+            if (cantidad > 10) {
+                cout << "¡Felicitaciones! Usted ha ganado una membresía con un descuento del 5% en su próxima compra." << endl;
+            }
             break;
-        case 4: cout<<"ud. eligio vip: "<<endl;
+        case 4:
+            cout << "Ud. eligió VIP." << endl;
             precio = 160;
-            cout <<"elija una bebida gaseosa: " <<endl;
-            cout <<"elija una bebida cerveza: " <<endl;
-            cout <<"elija una bebida agua: " <<endl;
-            cin >> bebida;
+            if (cantidad * precio > 330) {
+                cout << "Lo sentimos, no puede elegir ninguna bebida porque la suma de las entradas VIP no supera los 330 soles." << endl;
+            } else {
+                cout << "Elija una bebida gaseosa: " << endl;
+                cout << "Elija una bebida cerveza: " << endl;
+                cout << "Elija una bebida agua: " << endl;
+                cin >> bebida;
+            }
             break;
         default:
             cout << "Opción inválida." << endl;
             return 0;
     }
-
-     total = precio * cantidad;
+    total = precio * cantidad;
     cout << "El total a pagar es: $" << total << endl;
-
-    if (ubicacion == 4) {
+    if (ubicacion == 4 && cantidad * precio <= 330) {
+        cout << "Lo sentimos, no puede elegir ninguna bebida porque la suma de las entradas VIP no supera los 330 soles." << endl;
+    } else if (ubicacion == 4) {
         cout << "Bebida seleccionada: " << bebida << endl;
-     } 
+    }
     return 0;
-}
-// MODIFIQUE EL EJERCICIO PARA EL CASO QUE SI LA SUMA DE LAS ENTRADAS VIP NO SUPERA LOS 330 SOLES, NO PODRAN ELEGIR NINGUNA BEBIDA
+}// MODIFIQUE EL EJERCICIO PARA EL CASO QUE SI LA SUMA DE LAS ENTRADAS VIP NO SUPERA LOS 330 SOLES, NO PODRAN ELEGIR NINGUNA BEBIDA
 //Y QUE SI COMPRAN ENTRADAS EN PLATEA SUPERIORES A 10, SE LES OTORGA UNA MENBRESIA CON UN DESCUENTO DEL 5% EN LA PROXIMA COMORA
